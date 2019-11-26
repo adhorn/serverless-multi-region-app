3cda044ecb895c73a6bdd4b0a472810583ab21df
Adrian Hornsby <adhorn@amazon.com>
2019-11-26 16:49:05 +0200

    Bug fix

--- setup_route53.sh
+++ setup_route53.sh
@@ -1,22 +1,22 @@
 DOMAIN=adhorn.me
 SUBDOMAIN=globalvpc.${DOMAIN}
-REGION1='us-west-2'
-REGION2='us-east-1'
+REGION1=us-west-2
+REGION2=us-east-1
 STACKNAME=globalAppwithVPC-dev
 PROFILE=adrian-private
 
-USWEST2DOMAIN=$(aws cloudformation describe-stacks --stack-name ${STACKNAME} --region ${REGION1} --output text --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue')
-USEAST1DOMAIN=$(aws cloudformation describe-stacks --stack-name ${STACKNAME} --region ${REGION2} --output text --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue')
+USWEST2DOMAIN=$(aws cloudformation describe-stacks --stack-name ${STACKNAME} --region ${REGION1} --output text --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue' | sed 's/https:\/\///g' | sed 's/\/dev//g')
+USEAST1DOMAIN=$(aws cloudformation describe-stacks --stack-name ${STACKNAME} --region ${REGION2} --output text --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue' | sed 's/https:\/\///g' | sed 's/\/dev//g')
 
-USWEST2REG=$(aws apigateway get-domain-name --domain-name ${SUBDOMAIN} --region ${REGION1} --query 'regionalDomainName')
+USWEST2REG=$(aws apigateway get-domain-name --domain-name ${SUBDOMAIN} --region ${REGION1} --query 'regionalDomainName') 
 USEAST1REG=$(aws apigateway get-domain-name --domain-name ${SUBDOMAIN} --region ${REGION2} --query 'regionalDomainName')
 
 
 # Create HealthChecks and tag them
-HEALTHCHECK1=$(aws route53 create-health-check  --caller-reference $(date "+%Y%m%d%H%M%S") --health-check-config Type=HTTPS,ResourcePath="/dev/health",FullyQualifiedDomainName="k90kby12nl.execute-api.us-west-2.amazonaws.com",RequestInterval=10,FailureThreshold=1 --profile ${PROFILE} --output text --query 'HealthCheck.Id')
+HEALTHCHECK1=$(aws route53 create-health-check  --caller-reference $(date "+%Y%m%d%H%M%S") --health-check-config Type=HTTPS,ResourcePath="/dev/health",FullyQualifiedDomainName=${USWEST2DOMAIN},RequestInterval=10,FailureThreshold=1 --profile ${PROFILE} --output text --query 'HealthCheck.Id')
 HEALTHCHECK1_TAG=$(aws route53 change-tags-for-resource --resource-type healthcheck --resource-id ${HEALTHCHECK1} --add-tags Key=Name,Value=${STACKNAME}-${REGION1} --profile ${PROFILE})
 
-HEALTHCHECK2=$(aws route53 create-health-check  --caller-reference $(date "+%Y%m%d%H%M%S") --health-check-config Type=HTTPS,ResourcePath="/dev/health",FullyQualifiedDomainName="k90kby12nl.execute-api.us-west-2.amazonaws.com",RequestInterval=10,FailureThreshold=1 --profile ${PROFILE} --output text --query 'HealthCheck.Id')
+HEALTHCHECK2=$(aws route53 create-health-check  --caller-reference $(date "+%Y%m%d%H%M%S") --health-check-config Type=HTTPS,ResourcePath="/dev/health",FullyQualifiedDomainName=${USEAST1DOMAIN},RequestInterval=10,FailureThreshold=1 --profile ${PROFILE} --output text --query 'HealthCheck.Id')
 HEALTHCHECK2_TAG=$(aws route53 change-tags-for-resource --resource-type healthcheck --resource-id ${HEALTHCHECK2} --add-tags Key=Name,Value=${STACKNAME}-${REGION2} --profile ${PROFILE})
 
 
